a8642ac58446c7721c94c33066e2fbce313fd608
yeonje <yeonje@localhost>
2024-12-29 15:39:31 +0900

    First commit in dev branch

--- MyProjects/src/MyProject/A.java
+++ MyProjects/src/MyProject/A.java
@@ -3,6 +3,7 @@ package MyProject;
 public class A {
 	public static void main(String[] args) {
 		System.out.println(" AAA ");
+		System.out.println(" devB Changed A ");
 		
 		B plus = new B();
 		plus.Plus();
